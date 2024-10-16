6a3b348a31725b35924ba603bfae2e9cc2250e61
Your Name <you@example.com>
2024-10-16 11:34:32 -0500

    created the logout functionality

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,12 +1,14 @@
-document.querySelector("#logout").addEventListener("click", async () => {
-  const response = await fetch("/api/users/logout", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#logout").addEventListener("click", async () => {
+    const response = await fetch("/api/users/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
 
-  if (response.ok) {
-    document.location.replace("/"); // Redirect to homepage after logging out
-  } else {
-    alert("Failed to log out.");
-  }
+    if (response.ok) {
+      document.location.replace("/"); // Redirect to homepage after logging out
+    } else {
+      alert("Failed to log out.");
+    }
+  });
 });
